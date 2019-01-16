f265917d016d048178115203df93292f3bdeeb2b
Winry4 <rebecamolinaprados@gmail.com>
2019-01-16 19:01:07 +0100

    con face

---
+++ PrácticaTySWeb/src/main/webapp/face.js
@@ -0,0 +1,98 @@
+$(function() {
+
+	var app_id = '771715253188022';
+	var scopes = 'email, user_friends';
+
+	var btn_login = '<a href="#" id="login" class="btn btn-primary">Iniciar sesión</a>';
+
+	var div_session = "<div id='facebook-session'>"+
+					  "<strong></strong>"+
+					  "<img>"+
+					  "<a href='#' id='logout' class='btn btn-danger'>Cerrar sesión</a>"+
+					  "</div>";
+
+	window.fbAsyncInit = function() {
+
+	  	FB.init({
+	    	appId      : app_id,
+	    	status     : true,
+	    	cookie     : true, 
+	    	xfbml      : true, 
+	    	version    : 'v3.1'
+	  	});
+
+
+	  	FB.getLoginStatus(function(response) {
+	    	statusChangeCallback(response, function() {});
+	  	});
+  	};
+
+  	var statusChangeCallback = function(response, callback) {
+  		console.log(response);
+   		
+    	if (response.status === 'connected') {
+      		getFacebookData();
+    	} else {
+     		callback(false);
+    	}
+  	}
+
+  	var checkLoginState = function(callback) {
+    	FB.getLoginStatus(function(response) {
+      		callback(response);
+    	});
+  	}
+
+  	var getFacebookData =  function() {
+  		FB.api('/me', function(response) {
+	  		$('#login').after(div_session);
+	  		$('#login').remove();
+	  		window.location = "elegirJuego.html"
+	  		$('#facebook-session img').attr('src','http://graph.facebook.com/'+response.id+'/picture?type=large');
+	  		sessionStorage.setItem("img", #facebook-session img);
+	  		sessionStorage.setItem("name", response.name);
+	  		
+	  	});
+  	}
+
+  	var facebookLogin = function() {
+  		checkLoginState(function(data) {
+  			if (data.status !== 'connected') {
+  				FB.login(function(response) {
+  					if (response.status === 'connected')
+  						getFacebookData();
+  				}, {scope: scopes});
+  			}
+  		})
+  	}
+
+  	var facebookLogout = function() {
+  		checkLoginState(function(data) {
+  			if (data.status === 'connected') {
+				FB.logout(function(response) {
+					$('#facebook-session').before(btn_login);
+					$('#facebook-session').remove();
+				})
+			}
+  		})
+
+  	}
+
+
+
+  	$(document).on('click', '#login', function(e) {
+  		e.preventDefault();
+
+  		facebookLogin();
+  	})
+
+  	$(document).on('click', '#logout', function(e) {
+  		e.preventDefault();
+
+  		if (confirm("¿Está seguro?"))
+  			facebookLogout();
+  		else 
+  			return false;
+  	})
+
+})
